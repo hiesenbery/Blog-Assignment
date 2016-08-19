95a6f4590295b7e86d3ac3e5670d029e797d5734
hiesenbery <kyew428@gmail.com>
2016-08-19 12:56:34 +0800

    added signOut function to be called by sign out button

---
+++ BlogAssingment/general.js
@@ -0,0 +1,18 @@
+﻿function signOut()
+{
+    alert("Signing out");
+    $.ajax({
+        type: "POST",
+        url: "GetData.aspx/SignOut",
+        data: "{}",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        success: function (response) {
+            alert("Success");
+        },
+        error: function (response) {
+            alert("Error: " + response.responseText);
+        }
+    });
+}
